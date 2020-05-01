ca6ff010c1be68aa00f61d94151816c0d151c0f9
Hashim Aslam <hashim14aslam@gmail.com>
2020-05-01 11:26:12 +0530

    pushing finished changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "../Pages/LandingPage/LandingPage";
 import Dashboard from "../Pages/Dashboard/Dashboard";
+import FinalDashboard from "../Pages/FinalDashboard/FinalDashboard";
 function App() {
   return (
     <div>
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route path="/user/dashboard" component={Dashboard} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/details" component={FinalDashboard} />
         </Switch>
       </Router>
     </div>
